f7cabaf6526e9a4ef9af1c9afa865b9ac812d946
Fahad J <Fahad_Javed008@yahoo.com>
2020-01-29 18:49:21 +0500

    Dynamic Pages with Templating

--- src/app.js
+++ src/app.js
@@ -4,15 +4,35 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Fahad Javed'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Fahad Javed'
+  });
+});
+
 //
-// Goal: Create two more HTML files
+// Goal: Create a template for the help page
 //
-// 1. Create a html page for about with "About" title
-// 2. Create a html page for help with "Help" title
-// 3. Remove the old route handlers for both
-// 4. Visit both in the browser to test your work
+// 1. Setup a help template to render a help message to the screen
+// 2. Setup the help route and render the template with an example message
+// 3. Visit the route in the browser and see your help message print
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: 'This is some helpful text.'
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
