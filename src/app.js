00d70d5f706947f067915a87eb3a12eb0d91ca63
Fahad J <Fahad_Javed008@yahoo.com>
2020-01-29 20:06:59 +0500

    Advanced Templating

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -30,7 +33,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    helpText: 'This is some helpful text.'
+    helpText: 'This is some helpful text.',
+    title: 'Help',
+    name: 'Fahad Javed'
   });
 });
 
@@ -44,3 +49,10 @@ app.get('/weather', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
 });
+
+//
+// Goal: Create a partial for the footer
+//
+// 1. Setup the template for the footer partial "Created by Some Name"
+// 2. Render the partial at the bottom of all three pages
+// 3. Test your work by visiting all three pages
